5a4540f0ef3ca857afa36ac4a32e01c8cb13bb69
John Diamond <johndiamond@gmx.co.uk>
2017-10-24 16:07:37 +0200

    chore: remove debug logs

--- test/message-parserSpec.ts
+++ test/message-parserSpec.ts
@@ -5,7 +5,7 @@ import {
 import { parse, parseData } from '../src/message-parser'
 import { MESSAGES } from '../src/messages'
 
-fdescribe('message parser', () => {
+describe('message parser', () => {
   for (const topic in MESSAGES) {
     for (const action in MESSAGES[topic]) {
       const spec = MESSAGES[topic][action]
@@ -17,10 +17,6 @@ fdescribe('message parser', () => {
         it (`parses ${TOPIC[topic]} messages ${action} correctly`, () => {
           const result = parse(spec.text.value)[0]
           expect(parseData(result)).toEqual(true)
-          if (result.topic === TOPIC.AUTH && action === 'REQUEST') {
-            console.log(result)
-            console.log(spec.message)
-          }
           delete result.data
           expect(result).toEqual(spec.message)
         })
